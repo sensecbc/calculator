117e8b7a8a4542f80011b840a22f4d8672d14bac
sensecbc <rtavares4@ua.pt>
2024-08-14 17:38:10 +0100

    added html, css and js files. also have the calculator design done plus some functions for sum, subtract, multiply and divid

---
+++ script.js
@@ -0,0 +1,31 @@
+let firstNumber;
+let operator;
+let lastNumber;
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+  if (operator === "+") {
+    return sum(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    return divide(firstNumber, secondNumber);
+  }
+}
